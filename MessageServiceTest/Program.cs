b34010c8f73f6afdf5ed03efb2154a5a58c0051f
Akintade Samson <Tade@Vmlearn.com>
2015-12-05 05:59:47 +0100

    Update Program.cs

--- MessageServiceTest/Program.cs
+++ MessageServiceTest/Program.cs
@@ -17,20 +17,16 @@ namespace MessageServiceTest
         static void Main(string[] args)
         {
             MessageServicesManager manager = new MessageServicesManager();
-            MandrillMessageService mandril = (MandrillMessageService)manager.CreateMessageService(MessageServiceType.Mandrill, "vLVQJR8PdCWJB2WJowBcig");
+            MandrillMessageService mandril = (MandrillMessageService)manager.CreateMessageService(MessageServiceType.Mandrill, "key");
             MandrillMessageInformation messageInfo = new MandrillMessageInformation();
-            messageInfo.message.from_email = "postmaster@transcademy.com";
-            messageInfo.message.from_name = "transcademy";
+            messageInfo.message.from_email = "";
+            messageInfo.message.from_name = "";
             messageInfo.message.subject = "testing...";
-            messageInfo.message.to.Add(new ToEmail() { email = "tdontop@yahoo.com", name ="tdontop@yahoo.com" , type = "to" });
+            messageInfo.message.to.Add(new ToEmail() { email = "", name ="" , type = "to" });
             messageInfo.template_name = "Notification";
             messageInfo.template_content.Add(new TemplateContent() { name = "paragraph_one", content = "Testing Testing..." });
             messageInfo.message.global_merge_vars.Add(new Merge() { name = "name", content = messageInfo.message.to[0].name });
             List<Response> responses = mandril.Message.Send(messageInfo);
-
-
-            //Regex rg = new Regex(@"^[a-z0-9]+(\.[a-z0-9]+)*@[a-z]+(\.[a-z]+)+$");
-            //var status= rg.IsMatch("768696shafagsg7696.co.co.uk@gmail");
             Console.ReadLine();
 
         }
